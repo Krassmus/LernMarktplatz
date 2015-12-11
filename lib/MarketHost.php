7ba0f401898443b0ce82b021245b6d29b23d2a89
Rasmus Fuhse <krassmus@gmail.com>
2015-12-11 21:44:49 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -100,8 +100,6 @@ class MarketHost extends MarketIdentity {
                 }
                 if (!$host->isMe()) {
                     $material_data['data']['foreign_material_id'] = $material_data['data']['id'];
-                    var_dump($material_data['data']);
-                    die();
                     $material = MarketMaterial::findOneBySQL("foreign_material_id = ? AND host_id = ?", array(
                         $material_data['foreign_material_id'],
                         $host->getId()
