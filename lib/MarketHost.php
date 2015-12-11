63e9954053498c4169bc74a2fba58b7e24b62c76
Rasmus Fuhse <krassmus@gmail.com>
2015-12-11 18:18:05 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -94,7 +94,8 @@ class MarketHost extends MarketIdentity {
                 $host = MarketHost::findOneByPublic_key($material_data['host']['public_key']);
                 echo $material_data['host']['public_key']."\n\n";
                 $extrahost = MarketHost::findOneBySQL("private_key IS NULL");
-                echo $extrahost['public_key'];
+                echo $extrahost['public_key']."\n\n";
+                var_dump($material_data['host']['public_key'] === $extrahost['public_key']);
                 die();
                 if (!$host) {
                     $host = new MarketHost();
