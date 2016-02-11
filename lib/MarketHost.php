fd2c7b41ea3e4c05f7eaa7f53592ff4dc09e88b2
Rasmus Fuhse <fuhse@data-quest.de>
2016-02-11 16:38:02 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -138,11 +138,11 @@ class MarketHost extends MarketIdentity {
         $data = studip_utf8encode($data);
         $payload = json_encode($data);
 
-        var_dump($payload);
-        die();
         $myHost = MarketHost::thisOne();
         $endpoint_url = $this['url']."push_data";
 
+        var_dump($payload);
+        die();
         $request = curl_init();
         curl_setopt($request, CURLOPT_URL, $this->getUrl($endpoint_url));
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
