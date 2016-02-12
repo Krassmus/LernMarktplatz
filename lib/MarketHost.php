3efabdb618b6629913ad76e374273061ff8b11d0
Rasmus Fuhse <fuhse@data-quest.de>
2016-02-12 09:32:10 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -156,7 +156,6 @@ class MarketHost extends MarketIdentity {
             "X-RASMUS: ".md5($myHost['public_key'])
         );
         echo($payload)."\n\n";
-        die();
         curl_setopt($request, CURLOPT_HTTPHEADER, $header);
 
         $result = curl_exec($request);
