b5503c7e44310be92a8175086dfee17b663c2c9c
Rasmus Fuhse <fuhse@data-quest.de>
2016-02-12 09:13:13 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -151,9 +151,6 @@ class MarketHost extends MarketIdentity {
         curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($request, CURLOPT_SSL_VERIFYHOST, false);
 
-        var_dump($myHost->verifySignature($payload, $myHost->createSignature($payload)));
-        die();
-        echo "\n\n";
         $header = array(
             "X-SIGNATURE: ".base64_encode($myHost->createSignature($payload)),
             "X-RASMUS: ".md5($myHost['public_key'])
