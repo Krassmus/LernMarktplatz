3a530682613fd5ab0407a28eb7229ee6b1574ce5
Rasmus Fuhse <fuhse@data-quest.de>
2016-02-16 15:31:22 +0100

    debugging

--- lib/MarketHost.php
+++ lib/MarketHost.php
@@ -158,6 +158,7 @@ class MarketHost extends MarketIdentity {
         curl_setopt($request, CURLOPT_HTTPHEADER, $header);
 
         $result = curl_exec($request);
+        var_dump($result);die();
         $response_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
         curl_close($request);
         return $response_code < 300;
