b5a40e43834c2b48d73d5645f506cfd08aaa892a
Rasmus Fuhse <krassmus@gmail.com>
2016-02-20 08:18:32 +0100

    deleting

--- views/market/details.php
+++ views/market/details.php
@@ -87,10 +87,10 @@
 <div style="text-align: center;">
     <? if (!$material['host_id'] && $material['user_id'] === $GLOBALS['user']->id) : ?>
         <?= \Studip\LinkButton::create(_("Bearbeiten"), PluginEngine::getURL($plugin, array(), "market/edit/".$material->getId()), array('data-dialog' => "1")) ?>
+        <form action="<?= PluginEngine::getLink($plugin, array(), "market/edit/".$material->getId()) ?>" method="post" style="display: inline;">
+            <?= \Studip\Button::create(_("Löschen"), "delete", array('value' => 1, 'onclick' => "return window.confirm('"._("Wirklich löschen?")."');")) ?>
+        </form>
     <? endif ?>
-    <form action="<?= PluginEngine::getLink($plugin, array(), "market/edit/".$material->getId()) ?>" method="post" style="display: inline;">
-        <?= \Studip\Button::create(_("Löschen"), "delete", array('value' => 1, 'onclick' => "return window.confirm('"._("Wirklich löschen?")."');")) ?>
-    </form>
 </div>
 
 
