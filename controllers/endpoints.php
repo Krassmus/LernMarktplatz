f6a94400a337f1d53eab85ba9006ac31490e7004
Rasmus Fuhse <krassmus@gmail.com>
2016-08-19 23:44:10 +0200

    work on

--- controllers/endpoints.php
+++ controllers/endpoints.php
@@ -401,7 +401,7 @@ class EndpointsController extends PluginController {
                     $data = studip_utf8decode(json_decode($body, true));
                     //$review = new LehrmarktplatzReview($review_id);
                     $review = LehrmarktplatzComment::findOneBySQL("comment_id = :id OR comment_id = :id", array('id' => $review_id));
-                    var_dump($review); die();
+                    var_dump($review); die(); 
                     if (!$review || $review->material['host_id'] !== $host->getId()) {
                         throw new Exception("Unknown material.");
                     }
