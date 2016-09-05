7eab7f64fe74e3006bb1ff23c96d5e3f72d5fd64
Rasmus Fuhse <fuhse@data-quest.de>
2016-09-05 09:23:48 +0200

    rename plugin class

--- LernMarktplatz.class.php
+++ LernMarktplatz.class.php
@@ -11,7 +11,7 @@ require_once __DIR__."/lib/LernmarktplatzComment.php";
 $GLOBALS['LERNMARKTPLATZ_HEADER_PUBLIC_KEY_HASH'] = "X-RASMUS";    //MD5-hash of the armored public key of the server
 $GLOBALS['LERNMARKTPLATZ_HEADER_SIGNATURE']       = "X-SIGNATURE"; //the base64 encoded signature provided by the public key over the body of the message
 
-class LehrMarktplatz extends StudIPPlugin implements SystemPlugin, ScorePlugin {
+class LernMarktplatz extends StudIPPlugin implements SystemPlugin, ScorePlugin {
 
     public function __construct() {
         parent::__construct();
