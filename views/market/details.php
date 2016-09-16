62417ca67e4b58219fa0c6c1cc6d58f329f4946d
Rasmus Fuhse <fuhse@data-quest.de>
2016-09-16 13:58:02 +0200

    remove bad link

--- views/market/details.php
+++ views/market/details.php
@@ -213,12 +213,13 @@
 
 <?
 Sidebar::Get()->setImage($plugin->getPluginURL()."/assets/sidebar-service.png");
-$actions = new ActionsWidget();
-$actions->addLink(
-    _("Eigenes Lernmaterial hochladen"),
-    PluginEngine::getURL($plugin, array(), "mymaterial/edit"),
-    Assets::image_path("icons/blue/add"),
-    array('data-dialog' => "1")
-);
-
-Sidebar::Get()->addWidget($actions);
+if ($GLOBALS['perm']->have_perm("autor")) {
+    $actions = new ActionsWidget();
+    $actions->addLink(
+        _("Eigenes Lernmaterial hochladen"),
+        PluginEngine::getURL($plugin, array(), "mymaterial/edit"),
+        Assets::image_path("icons/blue/add"),
+        array('data-dialog' => "1")
+    );
+    Sidebar::Get()->addWidget($actions);
+}
