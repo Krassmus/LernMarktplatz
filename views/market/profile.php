95e8310e12ec98333a7b7f9175db7cdce0f2c449
Rasmus Fuhse <krassmus@gmail.com>
2016-10-05 19:18:51 +0200

    working

--- views/market/profile.php
+++ views/market/profile.php
@@ -1,8 +1,8 @@
-<div style="display: flex;">
+<div style="display: flex; width: 100%; margin-bottom: 20px;">
     <div>
         <img src="<?= htmlReady($user['avatar']) ?>">
     </div>
-    <div>
+    <div style="width: 100%; padding-left: 10px;">
         <h1><?= htmlReady($user['name']) ?></h1>
         <div>
             <?= formatReady($user['description']) ?>
@@ -10,15 +10,17 @@
     </div>
 </div>
 
-<section class="contentbox">
-    <header>
-        <h1>
-            <?= Assets::img("icons/16/blue/service") ?>
-            <?= _("Lernmaterialien") ?>
-        </h1>
-    </header>
-    <section>
-        <?= $this->render_partial("mymaterial/_material_list.php", array('materialien' => $materials)) ?>
+<? if (count($materials)) : ?>
+    <section class="contentbox">
+        <header>
+            <h1>
+                <?= Assets::img("icons/16/blue/service") ?>
+                <?= _("Lernmaterialien") ?>
+            </h1>
+        </header>
+        <section>
+            <?= $this->render_partial("mymaterial/_material_list.php", array('materialien' => $materials)) ?>
+        </section>
     </section>
-</section>
+<? endif ?>
 
