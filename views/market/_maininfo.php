856f65c17c0e9aeb979f12cd00f484a257b2085d
Rasmus Fuhse <krassmus@gmail.com>
2017-08-04 22:09:00 +0200

    add to tools

--- views/market/_maininfo.php
+++ views/market/_maininfo.php
@@ -7,7 +7,7 @@
     <div style="text-align: center;">
         <img src="<?= $this->plugin->getPluginURL() ?>/assets/info_image.svg" style="max-width: 40%;">
     </div>
-    Ãœbungszettel, MusterlÃ¶sungen, Vorlesungsmitschriften oder gar Folien, selbsterstellte Lernkarteikarten oder alte
+    Übungszettel, Musterlösungen, Vorlesungsmitschriften oder gar Folien, selbsterstellte Lernkarteikarten oder alte
     Klausuren. Das alles wird einmal erstellt und dann meist vergessen. Auf dem Lernmaterialienmarktplatz bleiben sie
-    erhalten. Jeder kann was hochladen und jeder kann alles herunterladen. Alle Inhalte hier sind frei verfÃ¼gbar fÃ¼r jeden.
+    erhalten. Jeder kann was hochladen und jeder kann alles herunterladen. Alle Inhalte hier sind frei verfügbar für jeden.
 </div>
