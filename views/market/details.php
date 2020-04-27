77aacfb6d3f91ec3d38ab9d43f2d57448cd62ebb
Rasmus Fuhse <krassmus@gmail.com>
2020-04-27 15:36:51 +0200

    add editing of root

--- views/market/details.php
+++ views/market/details.php
@@ -162,9 +162,7 @@
 
 <div style="text-align: center;">
     <? if (!$material['host_id'] && ($material->isMine() || $GLOBALS['perm']->have_perm("root"))) : ?>
-        <? if ($material->isMine()) : ?>
-            <?= \Studip\LinkButton::create(_("Bearbeiten"), PluginEngine::getURL($plugin, array(), "mymaterial/edit/".$material->getId()), array('data-dialog' => "1")) ?>
-        <? endif ?>
+        <?= \Studip\LinkButton::create(_("Bearbeiten"), PluginEngine::getURL($plugin, array(), "mymaterial/edit/".$material->getId()), array('data-dialog' => "1")) ?>
         <form action="<?= PluginEngine::getLink($plugin, array(), "mymaterial/edit/".$material->getId()) ?>" method="post" style="display: inline;">
             <?= \Studip\Button::create(_("Löschen"), "delete", array('value' => 1, 'onclick' => "return window.confirm('"._("Wirklich löschen?")."');")) ?>
         </form>
